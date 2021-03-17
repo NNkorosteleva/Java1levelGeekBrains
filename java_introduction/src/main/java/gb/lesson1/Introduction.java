c56f04a2258928b562aab0bed0a9e5a298e567d1
akorosteleva <akorosteleva@tsconsulting.ru>
2021-03-17 12:07:20 +0300

    [Разработчик Java][1 Четверть][Java.Уровень 1]: добавлен блок тестирования решений заданий в метод main; добавлены 2 метода для ввода параметров с клавиатуры

--- java_introduction/src/main/java/gb/lesson1/Introduction.java
+++ java_introduction/src/main/java/gb/lesson1/Introduction.java
@@ -1,13 +1,51 @@
 package gb.lesson1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Introduction {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         var example = new Introduction();
-
+        /**
+         * тестирование 1 задания
+         */
+        System.out.print("a = ");
+        int a = setVariableInt();
+        System.out.print("b = ");
+        int b = setVariableInt();
+        System.out.print("c = ");
+        int c = setVariableInt();
+        System.out.print("d = ");
+        int d = setVariableInt();
+        System.out.println("a * (b + (c / d)) = " + example.expression(a,b,c,d));
+        /**
+         * тестирование 2 задания
+         */
+        System.out.print("a = ");
+        a = setVariableInt();
+        System.out.print("b = ");
+        b = setVariableInt();
+        System.out.println("Сумма двух чисел лежит в интервале [10,20]? " + example.check_interval(a,b));
+        /**
+         * тестирование 3 задания
+         */
+        System.out.print("a = ");
+        a = setVariableInt();
+        System.out.println("Число положительное? " + example.check_positive_v1(a) + " " + example.check_positive_v2(a));
+        /**
+         * тестирование 4 задания
+         */
+        System.out.print("name = ");
+        String name = setVariableString();
+        System.out.println(example.welcoming(name));
+        /**
+         * тестирование 5 задания
+         */
+        System.out.print("год = ");
+        a = setVariableInt();
+        System.out.println(example.check_leap_year_v1(a)+ " " + example.check_leap_year_v2(a));
     }
     /**
      * 1. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат с плавающей точкой,
@@ -27,6 +65,9 @@ public class Introduction {
      * 3. Написать метод, которому в качестве параметра передается целое число,
      * метод должен проверить положительное ли число передали, или отрицательное.
      * Замечание: ноль считаем положительным числом. Результат работы метода вывести в консоль
+     *
+     * check_positive_v1 - выводит тексовое сообщение
+     * check_positive_v2 - вывод true, если число положительное, false - отрицательное
      */
     public String check_positive_v1(int a){
         return a>=0?"число положительное":"число отрицательное";
@@ -45,11 +86,26 @@ public class Introduction {
      * 5.Написать метод, который определяет является ли год високосным. Каждый 4-й год является високосным,
      * кроме каждого 100-го, при этом каждый 400-й – високосный.
      * Для проверки работы вывести результаты работы метода в консоль
+     *
+     * check_leap_year_v1 - выводит тексовое сообщение
+     * check_leap_year_v2 - вывод true, если год високосный, false - не високосный год
      */
     public String check_leap_year_v1(int year){
         return ((year % 100 != 0 || year % 400 == 0) && year % 4 == 0)? year + " - високосный год":year + " - не високосный год";
     }
     public boolean check_leap_year_v2(int year){
         return (year % 100 == 0 )?(year % 400 == 0):(year % 4 == 0);
+    }
+    /**
+     * статичный метод setVariableInt для ввода с клавиатуры целого числа
+     * статичный метод setVariableString для ввода с клавиатуры символьной строки
+     */
+    public static int setVariableInt() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
+    }
+    public static String setVariableString() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
     }
 }
