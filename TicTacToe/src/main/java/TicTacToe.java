2d95d81498e54e8b6bd0845ec445d385adaa3255
akorosteleva <akorosteleva@tsconsulting.ru>
2021-04-01 10:39:21 +0300

    [Разработчик Java][1 Четверть][Java.Уровень 1]: добавлены методы проверки победы и пустоты поля; в main сделана сборка всех методов для отладки игры; в метод блокировки хода игрока добавлено условие благодаря которому цик не уходит в бесконечность

--- TicTacToe/src/main/java/TicTacToe.java
+++ TicTacToe/src/main/java/TicTacToe.java
@@ -10,28 +10,38 @@ public class TicTacToe {
     public static final char DOT_EMPTY = '•';
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
-    public static final int SIZE = 10;
+    public static final int SIZE = 5;
     public static char[][] mapGame;
     public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     public static Random random = new Random();
 
     public static void main(String[] args) throws IOException {
 
-        extracted();
-
-    }
-
-    private static void extracted() throws IOException {
         TicTacToe toe = new TicTacToe();
         toe.initMap();
-        toe.printMap();
-
-        for (int i = 0; i < 4; i++) {
+        while (true) {
             toe.humanTurn();
             toe.printMap();
+            if (toe.checkWin(DOT_X)) {
+                System.out.println("Победил человек!");
+                break;
+            }
+            if (toe.mapFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
             toe.aiTurn();
             toe.printMap();
+            if (toe.checkWin(DOT_O)) {
+                System.out.println("Победил компьютер!");
+                break;
+            }
+            if (toe.mapFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
         }
+
     }
 
     /**
@@ -61,6 +71,18 @@ public class TicTacToe {
             }
             System.out.println();
         }
+    }
+
+    /**
+     * Метод mapFull проверка поля на полноту
+     */
+    public boolean mapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (mapGame[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -121,6 +143,12 @@ public class TicTacToe {
                     y = mapGame.length - x - 1;
                 } while (!checkValid(x, y));
                 break;
+            default:
+                do {
+                    x = random.nextInt(SIZE);
+                    y = random.nextInt(SIZE);
+                } while (!checkValid(x, y));
+                break;
         }
         mapGame[y][x] = DOT_O;
 
@@ -134,54 +162,94 @@ public class TicTacToe {
      */
     private int[] checkTurnUser() {
         int[] result = {0, 0};
-        int count = 0;
+        int countX, countEmpty;
         int countGlobal = 0;
         // Создаем стрим из строк mapGame - ищем в какой строке наибольшее кол-во 'X'
-        Stream<Character> charStream;
+        Stream<Character> charStream1, charStream2;
         for (int i = 0; i < mapGame.length; i++) {
-            charStream = new String(mapGame[i]).chars().mapToObj(x -> (char) x);
-            count = (int) charStream.filter(x -> x.equals('X')).count();
-            if (countGlobal < count) {
-                result[0] = 0;
+            charStream1 = new String(mapGame[i]).chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_X
+            charStream2 = new String(mapGame[i]).chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_EMPTY
+            countX = (int) charStream1.filter(x -> x.equals(DOT_X)).count();
+            countEmpty = (int) charStream2.filter(x -> x.equals(DOT_EMPTY)).count();
+            if (countGlobal < countX) {
+                result[0] = (countEmpty > 0) ? 0 : -1;
                 result[1] = i;
-                countGlobal = count;
+                countGlobal = countX;
             }
         }
         // Создаем стрим из столбцов mapGame - ищем в каком столбце наибольшее кол-во 'X'
         for (int i = 0; i < mapGame.length; i++) {
-            Stream.Builder<Character> charStreamBuilderV = Stream.<Character>builder();
+            Stream.Builder<Character> charStreamBuilderV1 = Stream.<Character>builder();
+            Stream.Builder<Character> charStreamBuilderV2 = Stream.<Character>builder();
             for (int j = 0; j < mapGame.length; j++) {
-                charStreamBuilderV.add(mapGame[j][i]);
-                charStreamBuilderV.toString().chars().mapToObj(x -> (char) x);
+                charStreamBuilderV1.add(mapGame[j][i]);
+                charStreamBuilderV1.toString().chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_X
+                charStreamBuilderV2.add(mapGame[j][i]);
+                charStreamBuilderV2.toString().chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_EMPTY
             }
-            count = (int) charStreamBuilderV.build().filter(x -> x.equals('X')).count();
-            if (countGlobal < count) {
-                result[0] = 1;
+            countX = (int) charStreamBuilderV1.build().filter(x -> x.equals(DOT_X)).count();
+            countEmpty = (int) charStreamBuilderV2.build().filter(x -> x.equals(DOT_EMPTY)).count();
+            if (countGlobal < countX) {
+                result[0] = (countEmpty > 0) ? 1 : -1;
                 result[1] = i;
-                countGlobal = count;
+                countGlobal = countX;
             }
         }
         // Создаем стрим из главных диагоналей mapGame - ищем в какой диагонали наибольшее кол-во 'X'
-        Stream.Builder<Character> charStreamBuilderD1 = Stream.<Character>builder();
-        Stream.Builder<Character> charStreamBuilderD2 = Stream.<Character>builder();
+        Stream.Builder<Character> charStreamBuilderD11 = Stream.<Character>builder();
+        Stream.Builder<Character> charStreamBuilderD12 = Stream.<Character>builder();
+        Stream.Builder<Character> charStreamBuilderD21 = Stream.<Character>builder();
+        Stream.Builder<Character> charStreamBuilderD22 = Stream.<Character>builder();
         for (int i = 0; i < mapGame.length; i++) {
-            charStreamBuilderD1.add(mapGame[i][i]);
-            charStreamBuilderD1.toString().chars().mapToObj(x -> (char) x);
-            charStreamBuilderD2.add(mapGame[i][mapGame.length - i - 1]);
-            charStreamBuilderD2.toString().chars().mapToObj(x -> (char) x);
+            charStreamBuilderD11.add(mapGame[i][i]);
+            charStreamBuilderD11.toString().chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_X для главной диагонали
+            charStreamBuilderD12.add(mapGame[i][mapGame.length - i - 1]);
+            charStreamBuilderD12.toString().chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_EMPTY для главной диагонали
+            charStreamBuilderD21.add(mapGame[i][i]);
+            charStreamBuilderD21.toString().chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_X для побочной диагонали
+            charStreamBuilderD22.add(mapGame[i][mapGame.length - i - 1]);
+            charStreamBuilderD22.toString().chars().mapToObj(x -> (char) x); // Поток для подсчета кол-ва DOT_EMPTY для побочной диагонали
         }
-        count = (int) charStreamBuilderD1.build().filter(x -> x.equals('X')).count();
-        if (countGlobal < count) {
-            result[0] = 2;
-            countGlobal = count;
+        countX = (int) charStreamBuilderD11.build().filter(x -> x.equals(DOT_X)).count();
+        countEmpty = (int) charStreamBuilderD12.build().filter(x -> x.equals(DOT_EMPTY)).count();
+        if (countGlobal < countX) {
+            result[0] = (countEmpty > 0) ? 2 : -1;
+            countGlobal = countX;
         }
-        count = (int) charStreamBuilderD2.build().filter(x -> x.equals('X')).count();
-        if (countGlobal < count) {
-            result[0] = 3;
-            countGlobal = count;
+        countX = (int) charStreamBuilderD21.build().filter(x -> x.equals(DOT_X)).count();
+        countEmpty = (int) charStreamBuilderD22.build().filter(x -> x.equals(DOT_EMPTY)).count();
+        if (countGlobal < countX) {
+            result[0] = (countEmpty > 0) ? 3 : -1;
+            countGlobal = countX;
         }
-
         return result;
     }
 
+    /**
+     * Метод checkWin проверка победы. Методу передается какую метку проверяем и если нашли комбинацию,
+     * то возвращаем true. Данный метод работает для любого размера поля - нужно лишь изменить константу SIZE и
+     * для любого кол-ва играков - нужно передать методу нужную метку
+     */
+    private boolean checkWin(char label) {
+        int countH, countV;
+        int countD1 = 0;
+        int countD2 = 0;
+        for (int i = 0; i < mapGame.length; i++) {
+            countH = 0;
+            countV = 0;
+            for (int j = 0; j < mapGame.length; j++) {
+                if (mapGame[i][j] == label) countH++;
+                if (mapGame[j][i] == label) countV++;
+            }
+            if (countH == SIZE) return true;
+            if (countV == SIZE) return true;
+
+            if (mapGame[i][i] == label) countD1++;
+            if (mapGame[i][mapGame.length - i - 1] == label) countD2++;
+        }
+        if (countD1 == SIZE) return true;
+        if (countD2 == SIZE) return true;
+
+        return false;
+    }
 }
